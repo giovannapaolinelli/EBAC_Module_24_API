bf58e5e76e91b5a4080daeba757b02daa18ac13e
Giovanna <giovanna.paolinelli@gmail.com>
2023-09-21 21:23:32 -0300

    fix contracts

--- cypress/contracts/addresses.contract.js
+++ cypress/contracts/addresses.contract.js
@@ -1,14 +1,14 @@
 const Joi = require ('joi')
 
-const addressSchema = Joi.object({
-    address_1: Joi.string(),
-    address_2: Joi.string(),
-    city: Joi.string(),
+const addressSchema = Joi.array().items({
+    address_1: Joi.string().allow(null),
+    address_2: Joi.string().allow(null),
+    city: Joi.string().allow(null),
     createdAt: Joi.string(),
-    customers: Joi.array().items(Joi.string()),
-    id: Joi.string(),
-    state: Joi.string(),
+    customers: Joi.array().items(Joi.string().allow(null)),
+    id: Joi.string().allow(null),
+    state: Joi.string().allow(null),
     updatedAt: Joi.string(),
-    zip: Joi.number() 
+    zip: Joi.number().allow(null)
 })
 export default addressSchema;
--- cypress/contracts/customers.contract.js
+++ cypress/contracts/customers.contract.js
@@ -1,43 +1,15 @@
 const Joi = require('joi')
 
-const customersSchema = Joi.object({
-  address: Joi.array().items({
-    address_1: Joi.string(),
-    address_2: Joi.string(),
-    city: Joi.string(),
-    createdAt: Joi.string(),
-    customers: Joi.array().items(Joi.string()),
-    id: Joi.string(),
-    state: Joi.string(),
-    updatedAt: Joi.string(),
-    zip: Joi.number()
-  }),
+const customersSchema = Joi.array().items({
+  address: Joi.object().keys({
+    id: Joi.string()
+    }).allow(null),
   createdAt: Joi.string(),
-  email: Joi.string(),
-  firstName: Joi.string(),
+  email: Joi.string().allow(null),
+  firstName: Joi.string().allow(null),
   id: Joi.string(),
-  lastName: Joi.string(),
-  orders: Joi.array().items({
-    createdAt: Joi.string(),
-    customer: Joi.string(),
-    discount: Joi.number(),
-    id: Joi.string(),
-    product: Joi.array().items({
-      createdAt: Joi.string(),
-      description: Joi.string(),
-      id: Joi.string(),
-      itemPrice: Joi.number(),
-      name: Joi.string(),
-      orders: Joi.array().items(Joi.string()),
-      updatedAt: Joi.string()
-    }),
-    quantity: Joi.number(),
-    totalPrice: Joi.number(),
-    updatedAt: Joi.string()
-  }),
-  phone: Joi.string(),
+  lastName: Joi.string().allow(null),
+  phone: Joi.string().allow(null),
   updatedAt: Joi.string()
-
-
 })
 export default customersSchema;
